6217c8acd8c39378d3e0ca8a463c2843ce726961
zheedong <35412648+zheedong@users.noreply.github.com>
2021-10-04 14:51:53 +0000

    mosaic_aug update

--- AI/data/mosaic_augmentation/mosaic_augmentation.py
+++ AI/data/mosaic_augmentation/mosaic_augmentation.py
@@ -1,23 +1,35 @@
 from genericpath import exists
 import os
-import cv2
+#import cv2
 import tqdm
 
-def setUpImageRoute():
-    dataRoot = input('Input the root directory of YOLO train image (You SHOULD have images and label folder)')
+def getImageRoot():
+    dataRoot = input('Input the root directory of YOLO train image (You SHOULD have images and label folder) : ')
     if(os.path.exists(f'{dataRoot}/images') & os.path.exists(f'{dataRoot}/labels')):
         print('Image Route Setup compilt')
         return dataRoot
     else:
         print('Invalid image route')
+        exit()
 
 def getTooLargeLabelImage(dataRoot):
-    imageDirectory = os.path(f'{dataRoot}/images')
-    labelDirectory = os.path(f'{dataRoot}/labels')
+    imageDirectory = '{dataRoot}/images'
+    labelDirectory = '{dataRoot}/labels'
 
-    # TODO : If label is too big compare to image size, collect the name of image and return as tuple
-    
+    dataLen = len(os.listdir(imageDirectory))
+    # TODO : If label is too big compare to image size, collect the name of image and return as list
+
+    for i in range(dataLen):
+        img = cv2.imwrite(imageDirectory/)
+
 
 def doMosaicWith4Images(dataRoot):
     # TODO : Resize Image and Label
     print('aa')
+
+def main():
+    dataRoot = getImageRoot()
+    getTooLargeLabelImage(dataRoot)
+
+if __name__ == '__main__':
+    main()
