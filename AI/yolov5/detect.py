f816c915f3114562520666c96a63eefe105b975b
j-mayo <86545225+j-mayo@users.noreply.github.com>
2021-10-07 14:43:05 +0000

    add warning.txt

--- AI/yolov5/detect.py
+++ AI/yolov5/detect.py
@@ -30,7 +30,8 @@ def detect(args):
     output_image_path = args.output_image_path
     weight_path = args.weight_path
     activeBlur = args.blur
-
+    # strength = args.strength
+    output_warning_path = args.output_warning_path
     model = torch.hub.load('ultralytics/yolov5', 'custom', path=weight_path)
     
     # Inference
@@ -41,8 +42,14 @@ def detect(args):
         # TODO : Blur
         print("Not yet!")
     else:
+        warn_list = []
+        warn_dict = {1: '방탄조끼', 6: '군복'}
         for xmin, ymin, xmax, ymax, conf, class_num in results.xyxy[0]:
-            if class_num == 1 or 6:
+            class_num = int(class_num)
+            if class_num == 1 or class_num == 6:
+                #print(class_num)
+                #print(type(class_num))
+                warn_list.append(warn_dict[class_num])
                 print("Military uniform or bulletproof vest detected. Pass mosaic")
                 continue
 
@@ -55,8 +62,9 @@ def detect(args):
 
             #cx = (xmin + xmax) / 2
             #cy = (ymin + ymax) / 2  # center value 지정
-            #xlen = (xmax - cx) * str / 100
-            #ylen = (ymax - cy) * str / 100
+            #s = (str / 100) ** 2
+            #xlen = (xmax - cx) * s
+            #ylen = (ymax - cy) * s
 
             #ymin_scaled = cy - ylen 
             #ymax_scaled = cy + ylen 
@@ -71,16 +79,24 @@ def detect(args):
             
             img[ymin: ymax, xmin: xmax] = src   # 원본 이미지에 적용
         cv2.imwrite(output_image_path, img)
+        warn_list = ','.join(list(set(warn_list)))
+        warn_text = f'{warn_list}이/가 감지되었습니다. 해당 사진이 보안에 저촉된다면 삭제해주시길 바랍니다.'
+        with open(output_warning_path, 'w') as f:
+            f.write(warn_text)
+
+
+
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--input_image_path', '-i')
 parser.add_argument('--output_image_path', '-o')
 parser.add_argument('--weight_path', '-w')
 parser.add_argument('--blur', '-b', action="store_true")
-# parser.add_argument('--strength', '-s', type='int', default=100, choices=[50, 75, 100])
+# parser.add_argument('--strength', '-s', type='int', default=100, choices=[50, 75, 100]) # test 후 결과에 따라 강도 조정 예정
+parser.add_argument('--output_warning_path', '-o2')
 
 # TODO: arg로 mosaic 강도를 입력받고, 그 만큼 면적을 줄여서 return
-# TODO: output_warning_path를 입력받아 군복, 방탄조끼 class가 포함되어 있을 시 경고문 전달?
+# TODO: output_warning_path를 입력받아 군복, 방탄조끼 class가 포함되어 있을 시 경고문 전달? 해결
 
 
 args = parser.parse_args()
