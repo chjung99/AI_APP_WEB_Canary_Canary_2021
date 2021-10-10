6e2181ad2d49e314b28f712a2595fd2bc25ee2cc
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-10-10 13:13:55 +0000

    splash page

---
+++ APP/myApp/lib/pages/splash_page.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+import 'package:praticesig/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  startTime() async {
+    var _duration = new Duration(seconds: 4);
+    return new Timer(_duration, navigationPage);
+  }
+
+  void navigationPage() {
+    Navigator.of(context).pushReplacementNamed('/HomeScreen');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      backgroundColor: Color(0xffFAF4C0),
+      body: new Center(
+        child: Container(
+          child: new Image.asset(
+            "assets/CANARYLOGO.jpg",
+          ),
+        ),
+      ),
+    );
+  }
+}
