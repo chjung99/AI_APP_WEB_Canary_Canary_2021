022217081c201e4795485daa4bdd66ace64f519b
Ubuntu <ubuntu@ip-172-31-19-146.us-east-2.compute.internal>
2021-10-10 06:33:52 +0000

    add authentication

--- AI/train_starter_django.py
+++ AI/train_starter_django.py
@@ -1,19 +1,30 @@
 import requests
 import argparse
+import getpass
 from multiprocessing import Process
 
-def train(args):
+def login(args):
+    username = input('username: ')
+    password = getpass.getpass('password: ')
+    
+    json = {'username': username, 'password': password}
+    r = eval(requests.post(args.login_url, json=json).text)
+    
+    return r['token']
+    
+
+def train(args, token):
     # url = 'http://52.14.108.141:8080/deeplearning/train'
     # file_id
     
     file = {'file': open(args.file_path, 'rb')}
-    r = requests.post(args.upload_url, files=file)
+    r = requests.post(args.upload_url, files=file, headers={'Authorization': f'Bearer {token}'})
     
-    print(r.text)
     
 parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--upload_url', '-u', default='http://52.14.108.141:8080/deeplearning/upload')
 parser.add_argument('--train_url', '-t', default='http://52.14.108.141:8080/deeplearning/train')
+parser.add_argument('--login_url', '-l', default='http://52.14.108.141:8080/account/login')
 parser.add_argument('--file_path', '-f')
 
 args = parser.parse_args()
@@ -22,12 +33,15 @@ def main(args):
     if args.file_path.split('.')[-1] != 'zip':
         print('unsupported file type')
         return
-    p = Process(target=train, args=(args,))
+    
+    token = login(args)
+    
+    p = Process(target=train, args=(args, token, ))
     p.start()
-    print('upload dataset...')
+    print(f'upload {args.file_path}...')
     p.join()
     
-    r = requests.post(args.train_url)
-    print(r.text)
+    r = eval(requests.post(args.train_url, headers={'Authorization': f'Bearer {token}'}).text)
+    print(f'train model with {r["file"]}')
 
 main(args)
