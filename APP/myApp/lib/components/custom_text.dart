af5ed969964d95477c9cf2cb46781125b64ceafd
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-10-12 14:30:33 +0000

    loading page UI 구성

--- APP/myApp/lib/components/custom_text.dart
+++ APP/myApp/lib/components/custom_text.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/color.dart';
 
-Text customText({required String text, required double size}) {
-  return Text(
-    text,
-    style: TextStyle(
-      fontSize: size,
-      fontWeight: FontWeight.bold,
-      color: primaryColor,
-      fontFamily: "BlackHanSans",
-    ),
+TextStyle CustomText({required double size}) {
+  return TextStyle(
+    fontSize: size,
+    fontWeight: FontWeight.bold,
+    color: primaryColor,
+    fontFamily: "BlackHanSans",
   );
 }
