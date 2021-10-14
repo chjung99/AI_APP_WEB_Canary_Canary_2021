05a27197460941de09fb01edd7f6bf8dbcb04006
zheedong <35412648+zheedong@users.noreply.github.com>
2021-10-14 14:34:04 +0000

    conflict solved

--- SERVER/instagrapi/async_utils_connect_test/utils/detect_images.py
+++ SERVER/instagrapi/async_utils_connect_test/utils/detect_images.py
@@ -1,13 +1,5 @@
 # TODO : image를 download 한 후, Canary_YOLOv5 에서 detect.py 돌리기
 # 처리 완료 했으면 이미지 삭제하기
-<<<<<<< HEAD
-=======
-from tqdm import tqdm
-
-# from utils.image_path import *
-from image_path import Roots
-
->>>>>>> 937933f30225b55376fe82b6fef17cddd233ed96
 import os
 
 from tqdm import tqdm
@@ -30,7 +22,7 @@ if __name__ == '__main__':
         print(path.dirname(path.dirname(path.dirname(path.dirname( path.dirname( path.abspath(__file__) ) )) )))
         sys.path.append(path.dirname(path.dirname(path.dirname(path.dirname( path.dirname( path.abspath(__file__) ) )) )))
         from AI.yolov5.detect import *
-        from SERVER.instagrapi.async_utils_connect_test.utils.image_path import *
+        from SERVER.instagrapi.async_utils_connect_test.utils.image_path import Roots
 
 def make_directory_save_images(user_output_path):
     path = f'{Roots.IMAGE_OUTPUT_ROOT}/{user_output_path}'
@@ -66,17 +58,13 @@ def detect_images():
         for j in tqdm(range(0, test_needed_photo_number)):
             print("== Photo Number : %d ==" % j)
             user_photo_path = f'{test_needed_user_list[i]}/{test_needed_photo_list[j]}'
-<<<<<<< HEAD
             IMAGE_INPUT_PATH = f'{IMAGE_DOWNLOAD_ROOT}/{user_photo_path}'
             IMAGE_OUTPUT_PATH = f'{IMAGE_OUTPUT_ROOT}/{user_photo_path}'
             WARNING_OUTPUT_PATH = f'{WARNING_OUTPUT_ROOT}/{user_photo_path}'+('.txt')
             LOG_OUTPUT_PATH = f'{LOG_OUTPUT_ROOT}/{user_photo_path}'+('.txt')
-=======
-
             IMAGE_INPUT_PATH = f'{Roots.IMAGE_DOWNLOAD_ROOT}/{user_photo_path}'
             IMAGE_OUTPUT_PATH = f'{Roots.IMAGE_OUTPUT_ROOT}/{user_photo_path}'
             WARNING_OUTPUT_PATH = f'{Roots.WARNING_OUTPUT_ROOT}/{user_photo_path}'+('.txt')
->>>>>>> 937933f30225b55376fe82b6fef17cddd233ed96
 
             args = detectArgs()
             args.input_image_path = f'{IMAGE_INPUT_PATH}'
