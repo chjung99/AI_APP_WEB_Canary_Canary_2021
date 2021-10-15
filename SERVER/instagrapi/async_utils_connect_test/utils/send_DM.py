307fed428bf2716220e7f6f62924bdd0e36dce2b
zheedong <35412648+zheedong@users.noreply.github.com>
2021-10-15 10:31:33 +0000

    send_DM update

--- SERVER/instagrapi/async_utils_connect_test/utils/send_DM.py
+++ SERVER/instagrapi/async_utils_connect_test/utils/send_DM.py
@@ -6,6 +6,14 @@ import json
 from utils.get_client import *
 from utils.image_path import *
 
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        from os import path
+        print(path.dirname(path.dirname(path.dirname(path.dirname( path.dirname( path.abspath(__file__) ) )) )))
+        sys.path.append(path.dirname(path.dirname(path.dirname(path.dirname( path.dirname( path.abspath(__file__) ) )) )))
+        from SERVER.instagrapi.async_utils_connect_test.utils.image_path import Roots
+
 '''
 # used in case of debug
 cl = Client()
@@ -13,10 +21,10 @@ get_logined_client(cl, id, password)
 '''
 
 def get_waiting_user_list():
-    return os.listdir(IMAGE_OUTPUT_ROOT)
+    return os.listdir(Roots.IMAGE_OUTPUT_ROOT)
 
 def get_waiting_DM_list(user_id):
-    return os.listdir(f'{IMAGE_OUTPUT_ROOT}/{user_id}')
+    return os.listdir(f'{Roots.IMAGE_OUTPUT_ROOT}/{user_id}')
 
 def remove_all_file(file_path):
     if os.path.exists(file_path):
@@ -27,9 +35,9 @@ def remove_all_file(file_path):
         return 'Directory Not Found'
 
 def delete_detected_images(user_id):
-    remove_all_file(f'{IMAGE_DOWNLOAD_ROOT}/{user_id}')
-    remove_all_file(f'{IMAGE_OUTPUT_ROOT}/{user_id}')
-    remove_all_file(f'{WARNING_OUTPUT_ROOT}/{user_id}')
+    remove_all_file(f'{Roots.IMAGE_DOWNLOAD_ROOT}/{user_id}')
+    remove_all_file(f'{Roots.IMAGE_OUTPUT_ROOT}/{user_id}')
+    remove_all_file(f'{Roots.WARNING_OUTPUT_ROOT}/{user_id}')
 
 def send_DM(cl):    # Instagram Client Login
     waiting_user_list = get_waiting_user_list()
@@ -39,7 +47,7 @@ def send_DM(cl):    # Instagram Client Login
         waiting_DM_list = get_waiting_DM_list(waiting_user_list[i])
         waiting_DM_list_length = len(waiting_DM_list)
         for j in tqdm(range(0, waiting_DM_list_length)):
-            cl.direct_send_photo(f'{IMAGE_OUTPUT_ROOT}/{waiting_user_list[i]}/{waiting_DM_list[j]}', user_ids = [waiting_user_list[i]])
+            cl.direct_send_photo(f'{Roots.IMAGE_OUTPUT_ROOT}/{waiting_user_list[i]}/{waiting_DM_list[j]}', user_ids = [waiting_user_list[i]])
             # user_ids : It should be list
         delete_detected_images(waiting_user_list[i])
 
