f380cde5934c3a729d8af213cda1223f5c9ed403
zheedong <35412648+zheedong@users.noreply.github.com>
2021-10-15 10:44:06 +0000

    send DM update

--- SERVER/instagrapi/async_utils_connect_test/utils/send_DM.py
+++ SERVER/instagrapi/async_utils_connect_test/utils/send_DM.py
@@ -26,6 +26,9 @@ def get_waiting_user_list():
 def get_waiting_DM_list(user_id):
     return os.listdir(f'{Roots.IMAGE_OUTPUT_ROOT}/{user_id}')
 
+def get_waiting_warning_list(user_id):
+    return os.listdir(f'{Roots.WARNING_OUTPUT_ROOT}/{user_id}'+('.txt'))
+
 def remove_all_file(file_path):
     if os.path.exists(file_path):
         for file in os.scandir(file_path):
@@ -46,8 +49,15 @@ def send_DM(cl):    # Instagram Client Login
     for i in tqdm(range(0, waiting_user_list_length)):
         waiting_DM_list = get_waiting_DM_list(waiting_user_list[i])
         waiting_DM_list_length = len(waiting_DM_list)
+
+        waiting_warning_list = get_waiting_warning_list(waiting_user_list[i])
+
         for j in tqdm(range(0, waiting_DM_list_length)):
+            with open(waiting_warning_list[i]):
+                warning_txt = open(f'{Roots.WARNING_OUTPUT_ROOT}/{waiting_user_list[i]}/{waiting_warning_list[j]}', 'r')
+
             cl.direct_send_photo(f'{Roots.IMAGE_OUTPUT_ROOT}/{waiting_user_list[i]}/{waiting_DM_list[j]}', user_ids = [waiting_user_list[i]])
+            cl.direct_send(warning_txt, user_ids = [waiting_user_list[i]])
             # user_ids : It should be list
         delete_detected_images(waiting_user_list[i])
 
