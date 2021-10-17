d31e3e4752f41fff67072a826188198228216b2f
giirafe <76638529+giirafe@users.noreply.github.com>
2021-10-17 22:43:11 +0000

    msg handling 문구 수정

--- APP(BE)/instagrapi/async_utils_connect_test/utils/send_DM.py
+++ APP(BE)/instagrapi/async_utils_connect_test/utils/send_DM.py
@@ -46,13 +46,14 @@ async def send_DM(cl):    # Instagram Client Login
     waiting_user_list_length = len(waiting_user_list)
 
     # IMAGE_OUTPUT_ROOT 폴더의 len = w_u_l_length
+    # OUTPUT_ROOT 폴더의 전체 유저 수에 대한 for문
     for i in tqdm(range(0, waiting_user_list_length)):
         # 해당 유저 폴더의 사진들 = waiting_DM_list 
         waiting_DM_list = get_waiting_DM_list(waiting_user_list[i])
-        # 유저 폴더의 사진들 개수 = w_D_l_length
-        waiting_DM_list_length = len(waiting_DM_list)
         # 해당 유저 경고문의 개수 
         waiting_warning_list = get_waiting_warning_list(waiting_user_list[i])
+        # 유저 폴더의 사진들 개수 = w_D_l_length
+        waiting_DM_list_length = len(waiting_DM_list)
 
         for j in tqdm(range(0, waiting_DM_list_length)):
 
