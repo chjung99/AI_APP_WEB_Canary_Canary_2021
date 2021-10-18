dfcef2cdc9eb0bf80c1bbb43f614ea12a03dbea8
hui-jae Ryu <54922625+HELLOHIDI@users.noreply.github.com>
2021-10-18 17:11:32 +0900

    Delete APP/myApp directory

--- APP/myApp/lib/color.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/rendering.dart';
-
-Color primaryColor = const Color(0xff6E9FED);
-Color secondaryColor = const Color(0xFF86A8E7);
-Color splashBackgroundColor = const Color(0xffFEFEE5);
--- APP/myApp/lib/size.dart
+++
@@ -1,3 +0,0 @@
-const double marginVerticalSize = 50;
-const double titleTextSize = 40;
-const double marginHorizontalSize = 24;
